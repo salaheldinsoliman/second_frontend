95421632d1cb5fbd83eb14e2e0f3af98faa859fc
Ahmad Sameh <salaheldin_sameh@aucegypt.edu>
2021-10-13 16:46:13 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+<div id="wrapper">
+<nav class="navbar is-dark">
+    <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"></router-link>
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
+            <span aria-hidden="true">Profile</span>
+            <span aria-hidden="true">Home</span>
+        </a>
+    </div>
+
+    <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
+        <div class="navbar-end">
+            <router-link to="/get_loan" class="navbar-item">Get a loan</router-link>
+            <router-link to="/loan_fund" class="navbar-item">Loan a fund</router-link>
+
+
+        </div>
+
+    </div>
+
+    <div class="navbar-item">
+        <div class="butttons">
+            <router-link to="/login" class="button is-light">
+            <span class="icon"><i class="fas fa-shopping-cart"></i></span> Log in
+            </router-link>
+            <router-link to="/signup" class="button is-success">Sign up</router-link>
+
+        </div>
+
+    </div>
+
+</nav>
+<section class="section"> 
+<router-view/>
+</section>
+  
+<footer>
+    <p>Copyright blnk &copy; 2021</p>
+</footer>
+
+</div>
+
+</template>
+
+<script>
+
+
+export default{
+    data(){
+        return {
+            showMobileMenu: false
+        }
+
+
+
+
+    }
+
+}
+</script>
+
+
+
+
+<style lang="scss">
+@import '../node_modules/bulma'; 
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home">
+      <section class="hero is-medium is-dark mb-6">
+        <div class="hero-body has-text-centered">
+            <p class="title mb-6">
+                Welcome to Blnk
+            </p>
+            <p class="subtitle">
+                  Buy now pay later
+            </p>
+        </div>
+    </section>
+  
+  <div class="columns is-multiline">
+      <div class="column is-12">
+          <h2 class="is-size-2 has-text-centered">All Loans</h2>
+      </div>
+  </div>
+
+  <div v-for="loan in all_loans" v-bind:key="loan.id" >
+
+    <p>{{ loan.amount }}</p>
+
+  </div>
+</div>
+</template>
+
+
+
+
+
+
+<script>
+
+import axios from 'axios'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  data(){
+    return{all_loans:[]}
+  },
+  components: {
+ 
+  },
+  mounted(){
+    this.get_all_loans()
+  },
+  methods: {
+    get_all_loans(){
+
+      axios.get('/api/v1/all_loans/')
+      .then(response =>{
+        this.all_loans=response.data
+      })
+      .catch(error=>{
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
